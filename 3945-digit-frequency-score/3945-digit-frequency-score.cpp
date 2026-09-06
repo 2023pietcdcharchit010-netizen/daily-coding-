01e98c18140aa634781508cadb1d0256c97b7675
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-09-06 10:52:59 +0530

    Time: 1 ms (22.88%), Space: 9.1 MB (31.47%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        int sum=0;
-        while(n>0) {
+        unordered_map<int,int> mp;
+// Extract Each digit and store their Freq
+        while(n>0){
             int rem=n%10;
+            mp[rem]++;
             n=n/10;
-            sum=sum+rem;
         }
-        return sum;
-   }
+// Calculate digit * its freq
+        int ans=0;
+        for(auto it :mp){
+            ans=ans+it.first*it.second;
+        }
+        return ans;
+    }
 };
